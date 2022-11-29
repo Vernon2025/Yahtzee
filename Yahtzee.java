34197772bf95a73937f9b70cf232071334aa190a
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-11-29 11:42:08 -0800

    Second

--- Yahtzee.java
+++ Yahtzee.java
@@ -7,24 +7,27 @@ public class Yahtzee extends Die6 {
   private Die6 Die4;
   private Die6 Die5;
   public Yahtzee() {
+    Die1 = new Die6();
+    Die2 = new Die6();
+    Die3 = new Die6();
+    Die4 = new Die6();
+    Die5 = new Die6();
+  }
+   public void roll() {
+    
+  }
+  public void roll(int dieNumber) {
     Die1.roll();
     Die2.roll();
     Die3.roll();
     Die4.roll();
     Die5.roll();
   }
-  public void roll(int dieNumber) {
+  public void summarize() {
       
   }
-  public int getValue() {
-    return value;
-  }
-  public void roll() {
-    this.value = (int)(Math.random() * 6) + 1;
-  }
-  public int rollAndGetValue() {
-    roll();
-    return getValue();
+  public void toString() {
+      
   }
 }
 
