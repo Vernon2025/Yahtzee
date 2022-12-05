3c9da8ea74433e6bc258476357e149e2124232e0
Vernon Pitney <vernon.pitney@mrjaffesclass.com>
2022-12-05 11:50:45 -0800

    Actually complete

--- Yahtzee.java
+++ Yahtzee.java
@@ -1,47 +1,47 @@
 
 public class Yahtzee {
-  private Die6 Die1;
-  private Die6 Die2;
-  private Die6 Die3;
-  private Die6 Die4;
-  private Die6 Die5;
+  private Die6 die1;
+  private Die6 die2;
+  private Die6 die3;
+  private Die6 die4;
+  private Die6 die5;
   private String stringValue;
   private int getValue;
   public Yahtzee() {
-    Die1 = new Die6();
-    Die2 = new Die6();
-    Die3 = new Die6();
-    Die4 = new Die6();
-    Die5 = new Die6();
+    die1 = new Die6();
+    die2 = new Die6();
+    die3 = new Die6();
+    die4 = new Die6();
+    die5 = new Die6();
   }
   public void roll(int dieNumber) {
     if (dieNumber == 1) {
-        Die1.roll();
+        die1.roll();
     }
     if (dieNumber == 2) {
-        Die2.roll();
+        die2.roll();
     }
     if (dieNumber == 3) {
-        Die3.roll();
+        die3.roll();
     }
     if (dieNumber == 4) {
-        Die4.roll();
+        die4.roll();
     }
     if (dieNumber == 5) {
-        Die5.roll();
+        die5.roll();
     }
   }
   public String toString() {
-      stringValue = "Dice values: " + Die1.getValue() + " " + Die2.getValue() + " " + Die3.getValue() + " " + Die4.getValue() + " " + Die5.getValue();
+      stringValue = "Dice values: " + die1.getValue() + " " + die2.getValue() + " " + die3.getValue() + " " + die4.getValue() + " " + die5.getValue();
       return stringValue;
   }
   public String summarize() {
       int[] score = {0, 0, 0, 0, 0, 0, 0};
-      score[Die1.getValue()]++;
-      score[Die2.getValue()]++;
-      score[Die3.getValue()]++;
-      score[Die4.getValue()]++;
-      score[Die5.getValue()]++;
+      score[die1.getValue()]++;
+      score[die2.getValue()]++;
+      score[die3.getValue()]++;
+      score[die4.getValue()]++;
+      score[die5.getValue()]++;
       return "1-" + score[1] + " , 2-" + score[2] + " , 3-" + score[3] + " , 4-" + score[4] + " , 5-" + score[5] + " , 6-" + score[6];
   }
 }
